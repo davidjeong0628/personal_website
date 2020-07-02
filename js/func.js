0ea39d364f4f4d29e3d14a18680b66305fc8ec36
David Jeong <55358988+davidjeong0628@users.noreply.github.com>
2020-07-02 09:25:08 -0700

    fixed navig position

--- js/func.js
+++ js/func.js
@@ -2,10 +2,23 @@ function hamburger() {
     var navig = document.getElementById('navig');
     var navigStyle = getComputedStyle(navig);
     var navDis = navigStyle.display;
+    var navPos = navPosition();
+
+    navig.style.top = navPos + "px";
 
     if (navDis == 'none') {
         navig.style.display = 'inline-block';
     } else {
         navig.style.display = 'none';
     }
+}
+
+function navPosition() {
+    var myHeader = document.getElementById("myHeader");
+    var headerStyle = getComputedStyle(myHeader);
+    var navPos = parseFloat(headerStyle.height) + parseFloat(headerStyle.marginBottom);
+
+    console.log(navPos);
+
+    return navPos;
 }
