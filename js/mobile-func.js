4290ff97037384731beff6fc388201adb4fae913
David Jeong <55358988+davidjeong0628@users.noreply.github.com>
2020-07-03 08:08:23 -0700

    project page changes

--- js/mobile-func.js
+++ js/mobile-func.js
@@ -1,6 +1,17 @@
-function hamburger() {
+/*
+* This file contains functions pertaining to phone-size screens.
+*/
+
+
+
+/*
+* Displays the navigation if it was not displayed. Hides it if it was displayed.
+* Calls on navPosition() to place the navigation right below the header.
+*/
+function displayNavigation() {
     var navig = document.getElementById('navig');
     var navigStyle = getComputedStyle(navig);
+
     var navDis = navigStyle.display;
     var navPos = navPosition();
 
@@ -13,12 +24,23 @@ function hamburger() {
     }
 }
 
+/*
+* Calculates the total height of the header to help determine the navigation's position.
+* Returns the calculated value.
+*/
 function navPosition() {
     var myHeader = document.getElementById("myHeader");
     var headerStyle = getComputedStyle(myHeader);
     var navPos = parseFloat(headerStyle.height) + parseFloat(headerStyle.marginBottom);
 
-    console.log(navPos);
-
     return navPos;
+}
+
+/*
+* Hides the navigation.
+*/
+function removeNav() {
+    var navig = document.getElementById('navig');
+
+    navig.style.display = 'none';
 }
